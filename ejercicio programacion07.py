71be5fbff32efcdd30cf1ff2f5f45cad4406974f
Lorenmejia <48398554+Lorenmejia@users.noreply.github.com>
2019-06-05 16:17:51 -1200

    Add files via upload

---
+++ ejercicio programacion07.py
@@ -0,0 +1,9 @@
+Comidas = ["Espagetis", "Carnes", "Pollo", "Chuleta", "Pescado", "Sopa", "Camarones", "Hamburguesa", "Pizza", "Sushi", "Ensalada", "Costias", "Tacos",  "Nachos", "Chapsui", "Pure", "Hotdog", "Tajadas", "Deditos", "Arepas"]
+Comidas favoritas = ["Espagetis", "Carnes", "Pollo", "Chuleta", "Pescado"]
+def imprimir_menu():
+	for comida in comidas:	
+		print(Comida)
+	for comida_favorita in comidas_favoritas:	
+		print(Comidas_favoritas)
+	
+imprimir_menu()
