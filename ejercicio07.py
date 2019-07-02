b6a28f4defe252268f22ec151df7d8191dad0510
Lorenmejia <48398554+Lorenmejia@users.noreply.github.com>
2019-07-01 13:10:29 -1200

    Add files via upload

---
+++ ejercicio07.py
@@ -0,0 +1,60 @@
+platos = ["sushi", "camarones", "filete", "chuleta"]
+
+Orden = []
+
+def imprimir_platos():
+	print("1 listar menu")
+	print("2 agragar pedido")
+	print("3 imprimir pedido")
+	print("4 salir")
+	valor = return("ingrese el numero de la accion que desea realizar")
+
+def listar_menu():
+		print()
+	print("listado de menu")
+	for producto in range (lend(menus)):
+		print(" {0} {1}".format(producto,menus[producto])
+		
+
+def agregar_pedido():
+	print()
+	print("creando un pedido")
+	mesa = input("numero de mesa")
+	producto = input("nombre de platillo")
+	pedidos.append("Meza No. {0} {1} platillo {1}".format(mesa, producto))
+
+def imprimir_pedido():
+	if pedidos;
+		for pedido in pedidos:
+		print(pedido)
+	else:
+		print("No hay pedidos ingresados")
+continuar = True
+
+while continuar:
+v_retornado=imprimir_menu()
+if int(v_retornado) == 1:
+	print()
+	print("listado de menu")
+	for producto in range (lend(menus)):
+		print(" {0} {1}".format(producto,menus[producto]) 
+elif int(v_retornado) == 2:
+	print()
+	print("creando un pedido")
+	mesa = input("numero de mesa")
+	producto = input("nombre de platillo")
+	pedidos.append("Meza No. {0} {1} platillo {1}".format(mesa, producto))
+elif int(v_retornado) == 3:
+	if pedidos;
+		for pedido in pedidos:
+		print(pedido)
+elif int(v_retornado) == 4:
+	Continuar = false
+else:
+	print("Opcion no controlada")
+
+
+
+	
+
+ 		
